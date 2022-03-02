0e2544d9f8248b9507ccef37929fd26e9ec48f05
Masein Modi <imasein@gmail.com>
2022-03-02 05:39:52 -0800

    added extra route

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -8,4 +8,8 @@ func routes(_ app: Application) throws {
     app.get("hello") { req -> String in
         return "Hello, world!"
     }
+    
+    app.get("swift") { req -> String in
+        return "Hello, swift!"
+    }
 }
