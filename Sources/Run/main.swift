6ef4e67de2e404955b5d3feef4287e79184e94ef
Masein Modi <imasein@gmail.com>
2022-03-02 23:33:00 -0800

    added chat functionality

--- Sources/Run/main.swift
+++ Sources/Run/main.swift
@@ -1,9 +1,74 @@
-import App
+//import App
+//import Vapor
+//
+//var env = try Environment.detect()
+//try LoggingSystem.bootstrap(from: &env)
+//let app = Application(env)
+//defer { app.shutdown() }
+//try configure(app)
+//try app.run()
+
 import Vapor
 
 var env = try Environment.detect()
-try LoggingSystem.bootstrap(from: &env)
 let app = Application(env)
-defer { app.shutdown() }
-try configure(app)
+
+defer {
+    app.shutdown()
+}
+var clientsSet = Set<Client>()
+var clientsArray: [Client] = []
+var currentClient: Client!
+
+app.webSocket("chat") { req, client in
+    // since our hash function is on connection, username is not important
+    currentClient = Client(username: "Kossher", connection: client)
+    
+    client.onClose.whenComplete { _ in
+        clientsSet.remove(currentClient)
+    }
+    
+    client.onText { _, text in
+        do {
+            guard let data = text.data(using: .utf8) else {
+                return
+            }
+            let incomingMessage = try JSONDecoder().decode(SubmittedChatMessage.self, from: data)
+            let outgoingMessage = ReceivingChatMessage(message: incomingMessage.message,
+                                                       sender: incomingMessage.sender,
+                                                       senderID: incomingMessage.senderID,
+                                                       receiver: incomingMessage.receiver)
+            currentClient = Client(username: incomingMessage.sender,
+                                   connection: currentClient.connection)
+            clientsSet.insert(currentClient)
+            let json = try JSONEncoder().encode(outgoingMessage)
+            guard let jsonString = String(data: json, encoding: .utf8) else {
+                return
+            }
+            print("Incomming message: \(incomingMessage)")
+            print("Outgoing message: \(outgoingMessage)")
+            for client in clientsSet {
+                if client.username == incomingMessage.sender ||
+                    client.username == incomingMessage.receiver {
+                    print("Client Username: \(String(describing: client.username))")
+                    client.connection.send(jsonString)
+                }
+            }
+        }
+        catch {
+            print(error)
+        }
+    }
+}
+
 try app.run()
+
+extension WebSocket: Hashable {
+    public static func == (lhs: WebSocket, rhs: WebSocket) -> Bool {
+        ObjectIdentifier(lhs) == ObjectIdentifier(rhs)
+    }
+    
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(ObjectIdentifier(self))
+    }
+}
